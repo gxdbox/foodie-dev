a32cf967017351667f8138f120f8fab226c488bf
gxdbox <gxd683@126.com>
2021-10-22 06:37:14 +0800

    页面跳转

--- foodie-dev-sso/src/main/java/com/imooc/controller/SSOController.java
+++ foodie-dev-sso/src/main/java/com/imooc/controller/SSOController.java
@@ -3,28 +3,27 @@ package com.imooc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class SSOController {
     final static Logger logger = LoggerFactory.getLogger(SSOController.class);
 
-    @GetMapping("/hello")
+    @GetMapping("/login")
     @ResponseBody
-    public Object hello() {
-        logger.debug("debug: hello~");
-        logger.info("info: hello~");
-        logger.warn("warn: hello~");
-        logger.error("error: hello~");
-        return "Hello World~";
+    public String login(String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
+     model.addAttribute("returnUrl",returnUrl);
+     //后续完善跳转判断 TODO
+        return "login";
     }
 
 }
